2c38633537c1867d298a67a59eb1ad11ad233fac
dnyaneshwarmagar <dnyaneshwarmagar276@gmail.com>
2024-04-26 12:54:43 +0530

    don2

--- src/components/Review.js
+++ src/components/Review.js
@@ -48,7 +48,7 @@ const Review = () => {
         <button className="prev-btn" onClick={prevReview}>Prev</button>
         <button className="next-btn" onClick={nextReview}>Next</button>
       </div>
-      <button className="random-btn" onClick={randomReview}>surprise Me</button>
+      <button className="random-btn" onClick={randomReview}>surprise me</button>
     </div>
   );
 };
